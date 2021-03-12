7cc861b155860cc8a97c37bd98084cb3b47db957
Yoshida Misaki <misaki.yoshida.0917@gmail.com>
2021-03-12 23:51:47 +0900

    database set

---
+++ database/migrations/2021_03_12_140848_create_collaborations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollaborationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('food_id');
+            
+            $table->foreign('food_id')->references('id')->on('foods');
+            $table->unsignedBigInteger('store_id');
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collaborations');
+    }
+}
