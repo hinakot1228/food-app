a2fcc8d423257ab1c5cae44eff2eba3f911d30ad
Imaisi-Takuya <i.t0504soccer@gmail.com>
2021-03-24 13:38:13 +0900

    web.php set

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,82 +10,56 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/index', function () {
+Route::get('/storesindex', function () {
+    return view('storesIndex');
+});
+Route::get('/', function () {
     return view('index');
 });
-Route::get('/storesindex', function () {
-    return view('stores.storesIndex');
-});
-Route::get('/index', function () {
-    return view('Index');
-});
 Route::get('/signup', function () {
-    return view('users.signup');
+    return view('signup');
 });
 Route::get('/signin', function () {
-    return view('users.signin');
+    return view('signin');
 });
 Route::get('/user', function () {
-    return view('users.user');
+    return view('user');
 });
 Route::get('/useredit', function () {
-    return view('users.userEdit');
+    return view('userEdit');
 });
-
 Route::get('/reviewcreate', function () {
-    return view('reviews.reviewCreate');
+    return view('reviewCreate');
 });
-
 Route::get('/storecreate', function () {
-    return view('stores.storeCreate');
+    return view('storeCreate');
 });
-
-Route::get('/stores', function () {
-    return view('stores');
-});
-
-Route::get('/store', function () {
-    return view('stores.store');
-});
-// Route::get('/store', function () {
-//     return view('store');
+// Route::get('/stores', function () {
+//     return view('stores.stores');
 // });
-
+Route::get('/store', function () {
+    return view('store');
+});
 Route::get('/foodsindex', function () {
     return view('foodsIndex');
 });
-
 // Route::get('/reviewcreate', function () {
 //     return view('reviewCreate');
 // });
-
-// レビュー作成画面の投稿機能
+// レビュー作成画面のcreate機能
 // create.blade.phpの見た目を表示するためのルーティング
 Route::get('/reviews/create', 'ReviewController@create')->name('reviews.create');
 // 保存処理
 Route::post('/reviews', 'ReviewController@store')->name('reviews.store');
-
 // お店登録画面のcreate機能
 // storeCreate.blade.phpの見た目を表示するためのルーティング
 Route::get('/stores/create', 'StoreController@create')->name('stores.create');
 // 保存処理
 Route::post('/stores', 'StoreController@store')->name('stores.store');
-
 // キーワード該当した料理一覧画面
-Route::get('/search', 'SearchController@index')->name('searches.index');
-
-// レビューの一覧機能
-Route::get('/store', 'ReviewController@index')->name('reviews.index');
-
-//お店画面のRead機能
-Route::get('/stores', 'StoreController@index')->name('stores.index');
-
-// 店舗の詳細画面
-// {}の中にはstore_idが入る
-Route::get('/stores/{store}', 'StoreController@show')->name('stores.show');
-
-
+Route::get('/foods', 'FoodController@index')->name('foods.index');
+// 検索機能
+Route::post('/foods', 'FoodController@search');
+// キーワード該当した料理一覧画面
+Route::get('/stores', 'StoreController@searchIndex')->name('stores.searchIndex');
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
