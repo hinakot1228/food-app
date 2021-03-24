3fc93be8eaf27f25c286de273023c43a3b783edf
Hinako-Tanaka <youngbird1228@gmail.com>
2021-03-24 13:18:03 +0900

    second commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            FoodsTableSeeder::class,
-          ]);
+        // $this->call([
+        //     FoodsTableSeeder::class,
+        //   ]);
     }
 }
