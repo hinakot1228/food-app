6766e76dd9ef8b9c354c69629d892668f875e9f0
Hinako-Tanaka <youngbird1228@gmail.com>
2021-03-25 13:46:37 +0900

    set

--- resources/views/searches/foodsIndex.blade.php
+++ resources/views/searches/foodsIndex.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>店一覧</title>
-    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('/assets/css/style.css')}}" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 <body>
     <div class="footerFixed">
@@ -59,6 +59,6 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="{{asset('/assets/js/pagetop.js')}}"></script>
+    <script src="{{secure_asset('/assets/js/pagetop.js')}}"></script>
 </body>
 </html>
